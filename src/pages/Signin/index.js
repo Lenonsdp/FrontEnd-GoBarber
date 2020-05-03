53cc81bea8fbe97ebc8094795b24ea6770997642
Lenon <lenon.paula@bling.com.br>
2020-05-03 17:49:15 -0300

    Estilização layouts

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+import Logo from '../../assets/logo.svg';
 
-// import { Container } from './styles';
+const schema = Yup.object().shape({
+    email: Yup.string()
+        .email('Insira um e-mail valido')
+        .required('O e-mail é obrigatório'),
+    password: Yup.string().required('Insira a senha')
+});
+
+function handleSubmit(data) {
+    console.tron.log(data);
+}
 
 export default function Signin() {
-    return <h1>Login</h1>;
+    return (
+        <>
+            <img src={Logo} alt="Logo" />
+            <Form schema={schema} onSubmit={handleSubmit}>
+                <Input name="email" type="email" placeholder="Seu e-mail" />
+                <Input name="password" type="password" placeholder="Sua senha" />
+                <button type="submit">Acessar</button>
+                <Link to="/register">Criar conta gratuita</Link>
+            </Form>
+        </>
+    );
 }
--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+import Logo from '../../assets/logo.svg';
 
-// import { Container } from './styles';
+const schema = Yup.object().shape({
+    name: Yup.string().required('Insira a senha'),
+    email: Yup.string()
+        .email('Insira um e-mail valido')
+        .required('O e-mail é obrigatório'),
+    password: Yup.string().required('Insira a senha')
+});
 
-export default function Signup() {
-    return <div />;
+export default function SignUp() {
+
+    function handleSubmit(data) {
+        console.tron.log(data);
+    }
+
+    return (
+        <>
+            <img src={Logo} alt="Logo" />
+            <Form schema={schema} onSubmit={handleSubmit}>
+                <Input name="name" placeholder="Nome compĺeto" />
+                <Input name="email" type="email" placeholder="Seu e-mail" />
+                <Input name="password" type="password" placeholder="Sua senha secreta" />
+
+                <button type="submit">Acessar</button>
+                <Link to="/">Já tenho Login</Link>
+            </Form>
+        </>
+    );
 }
