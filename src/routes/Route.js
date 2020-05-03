364e94559575e3fb3f6e71ceae875aeef5aef6b1
Lenon <lenon.paula@bling.com.br>
2020-05-02 21:51:40 -0300

    Estilização e criação de layouts

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -2,7 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
-export default function RouteWapper({
+import AuthLayout from '../pages/_layouts/auth';
+import DefaultLayout from '../pages/_layouts/default';
+
+export default function RouteWrapper({
     component: Component,
     isPrivate,
     ...rest
@@ -17,16 +20,26 @@ export default function RouteWapper({
         return <Redirect to="/dashboard" />;
     }
 
+    const Layout = signed ? DefaultLayout : AuthLayout;
+
     // eslint-disable-next-line react/jsx-props-no-spreading
-    return <Route {...rest} component={Component} />;
+    return (
+        <Route {...rest}
+            render={props =>(
+                <Layout>
+                    <Component {...props} />
+                </Layout>
+            )}
+        />
+    );
 }
 
-RouteWapper.prototype = {
+RouteWrapper.prototype = {
     component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
         .isRequired,
     isPrivate: PropTypes.bool,
 };
 
-RouteWapper.defaultProps = {
+RouteWrapper.defaultProps = {
     isPrivate: false,
 };
