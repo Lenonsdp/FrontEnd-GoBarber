263aa97de8b84d4b6fece2215f0360e497393045
Lenon <lenon.paula@bling.com.br>
2020-05-05 22:27:07 -0300

    Ajuste reducer e criação de novo user reducer

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
+
+import { signInRequest } from '../store/modules/auth/actions';
 import Logo from '../../assets/logo.svg';
 
 const schema = Yup.object().shape({
@@ -11,11 +15,13 @@ const schema = Yup.object().shape({
     password: Yup.string().required('Insira a senha')
 });
 
-function handleSubmit(data) {
-    console.tron.log(data);
-}
-
 export default function Signin() {
+    const dispatch = useDispatch();
+
+    function handleSubmit({ email, password }) {
+        dispatch(signInRequest(email, password));
+    }
+
     return (
         <>
             <img src={Logo} alt="Logo" />
