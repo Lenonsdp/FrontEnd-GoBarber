717281a3ce6ee71f7dedadacd4aa0ce32d2f9a12
Lenon <lenon.paula@bling.com.br>
2020-05-20 22:33:49 -0300

    Estilização page profile reducers e action

--- src/pages/store/modules/user/sagas.js
+++ src/pages/store/modules/user/sagas.js
@@ -1,3 +1,26 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+import api from '../../../../services/api';
+import { updateProfileSuccess, updateProfilefailure } from './actions';
 
-export default all([]);
+export function* updateProfile({ payload }) {
+    try {
+        const { name, email, ...rest } = payload.data;
+
+        const profile = Object.assign(
+            { name, email },
+            rest.oldPassword ? rest : {}
+        );
+
+        const response = yield call(api.put, 'users', profile);
+
+        toast.success('Usuario atualizado com sucesso');
+
+        yield put(updateProfileSuccess(response.data));
+    } catch (error) {
+        toast.error('Usuário não atualizado, verifique seus dados e tente novemente');
+        yield put(updateProfilefailure());
+    }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
