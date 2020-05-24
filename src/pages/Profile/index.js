3c7d94aa09592d1343b2aed32affee4de4341e80
Lenon <lenon.paula@bling.com.br>
2020-05-24 16:47:17 -0300

    Logout da aplicação

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 import { Container } from './styles';
+import { signOut } from '../store/modules/auth/actions';
 import { updateProfileRequest } from '../store/modules/user/actions';
 import Avatar from './AvatarInput/index';
+import { signIn } from '../store/modules/auth/sagas';
 
 export default function Profile() {
     const dispatch = useDispatch();
@@ -14,6 +16,9 @@ export default function Profile() {
         dispatch(updateProfileRequest(data));
     }
 
+    function handleSignOut() {
+        dispatch(signOut());
+    }
     return (
         <Container>
             <Form initialData={profile} onSubmit={handleSubmit}>
@@ -27,7 +32,7 @@ export default function Profile() {
                 <Input name="confirmPassword" type="password" placeholder="Confirmação de senha" />
                 <button type="submit">Atualizar perfil</button>
             </Form>
-            <button type="button">Sair do Gobarber</button>
+            <button type="button" onClick={handleSignOut}>Sair do Gobarber</button>
         </Container>
     );
 }
